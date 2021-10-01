fed7d9c465faa641b50d338803c966338dcbb909
Premanshu Patel <pmpatel@udel.edu>
2021-09-30 23:41:59 -0400

    added comments

--- js/contact.js
+++ js/contact.js
@@ -4,15 +4,18 @@
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
+        // Send email to Premanshu Patel
         emailjs.sendForm('service_rr2dsgn', 'template_014ciz5', this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {
                 console.log('FAILED...', error);
             });
+        // Clear text from text fields
         this.forEach(input => {
             input.value = ''
         });
+        // Alert user when message is sent
         alert("Message was sent!")
         
     });
